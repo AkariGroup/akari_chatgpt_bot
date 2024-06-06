85ad502a4ca2fa045bda8f32a073d24dcc7f19b6
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2024-06-06 23:17:48 +0900

    Add set param method

--- lib/style_bert_vits.py
+++ lib/style_bert_vits.py
@@ -3,6 +3,7 @@ import time
 import wave
 from queue import Queue
 from threading import Thread
+from typing import Optional
 
 import pyaudio
 from urllib.request import urlopen
@@ -10,6 +11,7 @@ from urllib.parse import urlencode
 from urllib.request import Request
 
 from .err_handler import ignoreStderr
+import json
 
 
 class TextToStyleBertVits(object):
@@ -31,6 +33,10 @@ class TextToStyleBertVits(object):
         self.finished = True
         self.voice_thread = Thread(target=self.text_to_voice_thread)
         self.voice_thread.start()
+        self.model_id = 0
+        self.length = 1.0
+        self.style = "Neutral"
+        self.style_weight = 1.0
 
     def __exit__(self) -> None:
         """音声合成スレッドを終了する。"""
@@ -77,23 +83,71 @@ class TextToStyleBertVits(object):
         while not self.finished:
             time.sleep(0.01)
 
+    def get_model_id_from_name(self, model_name: str) -> int:
+        """
+        モデル名からモデル番号を取得する。
+
+        Args:
+            model_name (str): モデル名。
+
+        Returns:
+            int: モデル番号。
+
+        """
+        headers = {
+            "accept": "application/json"
+        }
+        address = "http://" + self.host + ":" + \
+            self.port + "/models/info"
+        # GETリクエストを作成
+        req = Request(address, headers=headers, method="GET")
+        with urlopen(req) as res:
+            model_info = res.read()
+            model_info_json = json.loads(model_info)
+            for key, details in model_info_json.items():
+                if model_name == details['id2spk']['0']:
+                    return key
+        raise ValueError("Model name not found")
+
+    def set_param(
+        self,
+        model_name: Optional[str] = None,
+        model_id: Optional[int] = None,
+        length: Optional[float] = None,
+        style: Optional[str] = None,
+        style_weight: Optional[float] = None,
+    ):
+        """
+        音声合成のパラメータを設定する。
+
+        Args:
+            model_name (str, optional): Style-Bert-VITS2のモデル名。デフォルトはNone。
+            model_id (int, optional): Style-Bert-VITS2のモデル番号。デフォルトはNone。
+            length (float, optional): 音声の再生速度。大きくする程読み上げ速度が遅くなる。デフォルトはNone。
+            style (str, optional): 音声の感情スタイル。デフォルトはNone。
+            style_weight (float, optional): 音声の感情スタイルの重み。値が大きいほど感情の影響が大きくなる。デフォルトはNone。
+
+        """
+        if model_name is not None:
+            self.model_id = self.get_model_id_from_name(model_name)
+        elif model_id is not None:
+            self.model_id = model_id
+        if length is not None:
+            self.length = length
+        if style is not None:
+            self.style = style
+        if style_weight is not None:
+            self.style_weight = style_weight
+
     def post_synthesis(
         self,
         text: str,
-        model_id: int = 0,
-        length: float = 1.0,
-        style: str = "Neutral",
-        style_weight: float = 1.0
     ) -> bytes:
         """
         Style-Bert-VITS2サーバーに音声合成要求を送信し、合成された音声データを取得する。
 
         Args:
             text (str): 音声合成対象のテキスト。
-            model_id (int, optional): Style-Bert-VITS2のモデル番号。デフォルトは0。
-            length (float, optional): 音声の再生速度。大きくする程読み上げ速度が遅くなる。デフォルトは1.0。
-            style (str, optional): 音声の感情スタイル。デフォルトは"Neutral"。
-            style_weight (float, optional): 音声の感情スタイルの重み。値が大きいほど感情の影響が大きくなる。デフォルトは1.0。
 
         Returns:
             Any: 音声合成クエリの応答。
@@ -104,9 +158,10 @@ class TextToStyleBertVits(object):
         }
         params = {
             "text": text,
-            "model_id": model_id,
-            "length": length,
-            "style": style
+            "model_id": self.model_id,
+            "length": self.length,
+            "style": self.style,
+            "style_weight": self.style_weight,
         }
         address = "http://" + self.host + ":" + \
             self.port + "/voice" + "?" + urlencode(params)
--- style_bert_vits_example.py
+++ style_bert_vits_example.py
@@ -22,10 +22,21 @@ def main() -> None:
     host = args.voice_host
     port = args.voice_port
     text_to_voice = TextToStyleBertVits(host, port)
+
+    # set_paramメソッドでモデル名や音声再生速度、感情スタイルなどを指定することができます。
+    # モデル名を指定
+    text_to_voice.set_param(model_name='jvnv-F1-jp')
+    # 音声再生速度を指定
+    # text_to_voice.set_param(length=2.0)
+    # 感情スタイルを指定
+    # text_to_voice.set_param(style='Happy')
+    # 感情スタイルの重みを指定
+    # text_to_voice.set_param(style_weight=3.0)
+
     print("発話させたい文章をキーボード入力後、Enterを押してください。")
     while True:
         text = input("Input: ")
-        text_to_voice.put_text(text)
+        text_to_voice.put_text(text=text, )
         print("")
 
 
