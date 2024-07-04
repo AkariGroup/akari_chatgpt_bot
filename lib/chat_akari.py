48fd7cb48bf4b3f4cef973d55e36f57623b69438
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2024-07-05 00:06:01 +0900

    Fix diff

--- lib/chat_akari.py
+++ lib/chat_akari.py
@@ -576,7 +576,8 @@ class ChatStreamAkari(object):
                                         sentence_index : sentence_index + pos + 1
                                     ]
                                     sentence_index += pos + 1
-                                    yield sentence
+                                    if sentence != "":
+                                        yield sentence
                                     break
 
     def chat_and_motion_anthropic(
