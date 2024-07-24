2b97aeb39402c8d311a4d1c7799bc83128de3f73
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2024-07-24 14:42:12 +0900

    Add conf

--- lib/conf.py
+++ lib/conf.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+GOOGLE_SPEECH_PROJECT_ID = os.environ.get("GOOGLE_SPEECH_PROJECT_ID")
 OPENAI_APIKEY = os.environ.get("OPENAI_API_KEY")
 ANTHROPIC_APIKEY = os.environ.get("ANTHROPIC_API_KEY")
 VOICEVOX_APIKEY = os.environ.get("VOICEVOX_API_KEY")
