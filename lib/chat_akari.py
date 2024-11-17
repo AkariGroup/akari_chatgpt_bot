55cc1ea57cbb728e6871ab412674634114643f3e
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2024-11-17 22:17:27 +0900

    Add claude models

--- lib/chat_akari.py
+++ lib/chat_akari.py
@@ -78,9 +78,14 @@ class ChatStreamAkari(object):
             "gpt-4-1106-vision-preview",
         ]
         self.anthropic_model_name = [
+            "claude-3-5-sonnet-20241022",
+            "claude-3-5-sonnet-latest",
             "claude-3-5-sonnet-20240620",
             "claude-3-opus-20240229",
+            "claude-3-opus-latest",
             "claude-3-sonnet-20240229",
+            "claude-3-5-haiku-20241022",
+            "claude-3-5-haiku-latest",
             "claude-3-haiku-20240307",
             "claude-2.1",
             "claude-2.0",
