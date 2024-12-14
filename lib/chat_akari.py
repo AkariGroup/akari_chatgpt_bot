831200563b7f90fc206497f019bf45a74a4d319d
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2024-12-14 22:13:47 +0900

    Add gemini-2.0-flash

--- lib/chat_akari.py
+++ lib/chat_akari.py
@@ -93,8 +93,10 @@ class ChatStreamAkari(object):
             "claude-instant-1.2",
         ]
         self.gemini_model_name = [
+            "gemini-2.0-flash-exp",
             "gemini-1.5-pro",
             "gemini-1.5-flash",
+            "gemini-1.5-flash-8b"
         ]
 
     def send_motion(self, name: str) -> None:
