97d8b0477772b9ca45cf5d0f43bec74cd3b9277b
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2025-06-18 08:50:08 +0900

    Update gemini 2.5 models

--- lib/chat.py
+++ lib/chat.py
@@ -100,12 +100,14 @@ class ChatStream(object):
             "claude-instant-1.2",
         ]
         self.gemini_model_name = [
+            "gemini-2.5-pro",
+            "gemini-2.5-pro-preview-06-05",
             "gemini-2.5-pro-preview-05-06",
-            "gemini-2.5-pro-exp-03-25",
+            "gemini-2.5-flash",
+            "gemini-2.5-flash-preview-05-20",
+            "gemini-2.5-flash-lite-preview-06-17",
             "gemini-2.0-pro-exp",
             "gemini-2.0-pro-exp-02-05",
-            "gemini-2.5-flash-preview-05-20",
-            "gemini-2.5-flash-preview-04-17",
             "gemini-2.0-flash",
             "gemini-2.0-flash-001",
             "gemini-2.0-flash-lite",
